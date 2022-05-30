95b21fd66d6bfa3c955bf84d312c941a3a93d9e9
lilsemann <lena@ahrlich-boettcher.com>
2022-05-30 11:04:07 +0200

    Vertragspartner

--- src/Kaufvertrag/dataLayer/businessObjects/Vertragspartner.java
+++ src/Kaufvertrag/dataLayer/businessObjects/Vertragspartner.java
@@ -1,5 +1,62 @@
 package Kaufvertrag.dataLayer.businessObjects;
 
-public class Vertragspartner {
+import Kaufvertrag.businessObjects.IAdresse;
+import Kaufvertrag.businessObjects.IVertragspartner;
 
+public class Vertragspartner implements IVertragspartner {
+    private String vorname;
+    private String nachname;
+    private String ausweisNr;
+    private IAdresse adresse;
+
+    public Vertragspartner(String vorname, String nachname) {
+        this.vorname = vorname;
+        this.nachname = nachname;
+    }
+
+
+    @Override
+    public int getId() {
+        return 0;
+    }
+
+    @Override
+    public String getVorname() {
+        return vorname;
+    }
+
+    @Override
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    @Override
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+
+    @Override
+    public String getNachname() {
+        return nachname;
+    }
+
+    @Override
+    public String getAusweisNr() {
+        return ausweisNr;
+    }
+
+    @Override
+    public void setAusweisNr(String ausweisNr) {
+        this.ausweisNr = ausweisNr;
+    }
+
+    @Override
+    public IAdresse getAdresse() {
+        return adresse;
+    }
+
+    @Override
+    public void setAdresse(IAdresse adresse) {
+        this.adresse = adresse;
+    }
 }
--- src/Kaufvertrag/dataLayer/dataAccessObjects/IVertragspartnerDao.java
+++ src/Kaufvertrag/dataLayer/dataAccessObjects/IVertragspartnerDao.java
@@ -1,4 +1,14 @@
 package Kaufvertrag.dataLayer.dataAccessObjects;
 
+import Kaufvertrag.businessObjects.IVertragspartner;
+
+import java.util.List;
+
 public interface IVertragspartnerDao {
+    public IVertragspartner create();
+    public IVertragspartner create(IVertragspartner vertragspartner);
+    public List<IVertragspartner> read();
+    public List<IVertragspartner> read(int id);
+    public void update(IVertragspartner vertragspartner);
+    public void delete(int id);
 }
