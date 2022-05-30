f2a3513e0dd4affc90a0bc4a5124c0a1616d27d5
noah <noah.thiering@web.de>
2022-05-30 10:46:27 +0200

    Ware, IWare

--- src/Kaufvertrag/businessObjects/IWare.java
+++ src/Kaufvertrag/businessObjects/IWare.java
@@ -1,4 +1,23 @@
 package Kaufvertrag.businessObjects;
 
+import java.util.List;
+
 public interface IWare {
+    public int getId();
+
+    public String getBezeichnung();
+    public void setBezeichnung(String bezeichnung);
+
+    public String getBeschreibung();
+    public void setBeschreibung(String beschreibung);
+
+    public double getPreis();
+    public void setPreis(double preis);
+
+    public List<String> getBesonderheiten();
+
+    public List<String> getMaengel();
+
+
+
 }
--- src/Kaufvertrag/dataLayer/businessObjects/Ware.java
+++ src/Kaufvertrag/dataLayer/businessObjects/Ware.java
@@ -1,5 +1,74 @@
 package Kaufvertrag.dataLayer.businessObjects;
 
-public class Ware {
+import Kaufvertrag.businessObjects.IWare;
 
+import java.util.List;
+
+public class Ware implements IWare {
+    private int id;
+    private String bezeichnung;
+    private String beschreibung;
+    private double preis;
+    private List<String> besonderheiten;
+    private List<String> maengel;
+
+
+    public Ware(String bezeichnung, double preis) {
+        this.bezeichnung = bezeichnung;
+        this.preis = preis;
+        this.beschreibung = "";
+        this.besonderheiten = null;
+        this.maengel = null;
+
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String toString() {
+        return bezeichnung + ": " + preis + "€" + "\nBeschreibung: " + beschreibung + "\nBesonderheiten: " + besonderheiten + "\nMängel: " + maengel;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String getBezeichnung() {
+        return bezeichnung;
+    }
+    @Override
+    public void setBezeichnung(String bezeichnung) {
+        this.bezeichnung = bezeichnung;
+    }
+
+    @Override
+    public String getBeschreibung() {
+        return beschreibung;
+    }
+    @Override
+    public void setBeschreibung(String beschreibung) {
+        this.beschreibung = beschreibung;
+    }
+
+    @Override
+    public double getPreis() {
+        return preis;
+    }
+    @Override
+    public void setPreis(double preis) {
+        this.preis = preis;
+    }
+
+    @Override
+    public List<String> getBesonderheiten() {
+        return besonderheiten;
+    }
+
+    @Override
+    public List<String> getMaengel() {
+        return maengel;
+    }
 }
