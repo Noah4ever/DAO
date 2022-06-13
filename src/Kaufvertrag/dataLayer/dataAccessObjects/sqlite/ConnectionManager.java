e93ff5e2d2cbff1a8e99a0a0a126bc6c1b85f0d0
Janis <janis.meister87@gmail.com>
2022-06-13 09:45:56 +0200

    sql

--- src/Kaufvertrag/dataLayer/dataAccessObjects/sqlite/ConnectionManager.java
+++ src/Kaufvertrag/dataLayer/dataAccessObjects/sqlite/ConnectionManager.java
@@ -1,25 +1,43 @@
 package Kaufvertrag.dataLayer.dataAccessObjects.sqlite;
 
-import com.sun.jdi.connect.spi.Connection;
 
-import java.sql.ResultSet;
-import java.sql.Statement;
+
+import java.sql.*;
+
+import static java.sql.DriverManager.getConnection;
+
 
 public class ConnectionManager {
 
-
+    private String CLASSNAME;
+    private String CONNECTIONSTRING;
     private Connection existingConnection;
     private boolean classLoaded;
 
-    public Connection getNewConnection() {
+    public static void main(String[] args) {
+        getNewConnection();
+    }
+    public static Connection getNewConnection() {
+        Connection connection = null;
+        // Connect to sqlite
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = (Connection) getConnection("jdbc:sqlite:db.sql");
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
 
+        return connection;
     }
 
+    /*
     public Connection getExistingConnection() {
 
     }
 
     public void close(ResultSet resultSet, Statement statement, Connection connection) {
 
-    }
+    }*/
 }
