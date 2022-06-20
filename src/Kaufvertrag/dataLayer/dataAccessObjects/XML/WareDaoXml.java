7a93081b5528fff53f1b1ef4dc8de79ca7583319
noah <noah.thiering@web.de>
2022-06-20 10:08:20 +0200

    Xml stuff

--- src/Kaufvertrag/dataLayer/dataAccessObjects/XML/WareDaoXml.java
+++ src/Kaufvertrag/dataLayer/dataAccessObjects/XML/WareDaoXml.java
@@ -4,24 +4,29 @@ import Kaufvertrag.businessObjects.IWare;
 import Kaufvertrag.dataLayer.dataAccessObjects.IWareDao;
 import Kaufvertrag.exceptions.DaoException;
 
+import java.io.IOException;
 import java.util.List;
 
 public class WareDaoXml implements IWareDao {
 
+    private String path = "D:\\.Downloads\\Programming\\IntelliJ\\projects\\DAO\\DAO\\src\\Kaufvertrag\\files\\test.xml";
 
     @Override
     public IWare create() {
-        return null;
+        ServiceXml serviceXml = new ServiceXml(path);
+        serviceXml.createWare();
     }
 
     @Override
     public IWare create(IWare ware) {
+        ServiceXml serviceXml = new ServiceXml(path);
+        serviceXml.createWareElement(ware);
         return null;
     }
 
     @Override
     public List<IWare> read() {
-        ServiceXml serviceXml = new ServiceXml("D:\\.Downloads\\Programming\\IntelliJ\\projects\\DAO\\DAO\\src\\Kaufvertrag\\files\\test.xml");
+        ServiceXml serviceXml = new ServiceXml(path;
         return serviceXml.Ware();
     }
 
@@ -37,12 +42,14 @@ public class WareDaoXml implements IWareDao {
     }
 
     @Override
-    public void update(IWare ware) throws DaoException {
-
+    public void update(IWare ware) throws DaoException, IOException {
+        ServiceXml serviceXml = new ServiceXml(path);
+        serviceXml.updateWare(ware);
     }
 
     @Override
-    public void delete(int id) throws DaoException {
-
+    public void delete(int id) throws DaoException, IOException {
+        ServiceXml serviceXml = new ServiceXml(path);
+        serviceXml.deleteWare(id);
     }
 }
